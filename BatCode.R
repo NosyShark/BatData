823ef35aa3e6a8405422010614358f7a9f895c5f
NosyShark <emmiswan@gmail.com>
2024-02-16 13:20:59 +0200

    Adding first bit of code to clear workspace and equipt right packages

---
+++ BatCode.R
@@ -0,0 +1,10 @@
+# This is an exercise to practice cleaning up data and looking at some preliminary results and keeping GitHub updated
+
+# clear workspace
+rm(list = ls())
+
+library(tidyverse)
+library(readxl)
+library(dplyr)
+library(ggplot2)
+
